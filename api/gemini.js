c53c15c3842055e3cdf232f7ce02cbe1e8a6d7f3
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-13 16:53:14 +0300

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -1,1 +1,95 @@
+// api/gemini.js
+// FashionAI Vercel Gemini Function
 
+
+export default async function handler(req, res) {
+
+
+    // CORS
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "*"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type"
+    );
+
+
+
+    if(req.method === "OPTIONS"){
+
+        return res.status(200).end();
+
+    }
+
+
+
+    try{
+
+
+        const { contents } = req.body;
+
+
+
+        const response =
+        await fetch(
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
+            {
+
+                method:"POST",
+
+                headers:{
+
+                    "Content-Type":"application/json",
+
+                    "x-goog-api-key":
+                    process.env.GEMINI_API_KEY
+
+                },
+
+
+                body:JSON.stringify({
+
+                    contents
+
+                })
+
+            }
+
+        );
+
+
+
+        const data =
+        await response.json();
+
+
+
+        return res.status(200).json(data);
+
+
+
+    }
+
+
+    catch(error){
+
+
+        console.error(
+            error
+        );
+
+
+        return res.status(500).json({
+
+            error:error.message
+
+        });
+
+
+    }
+
+
+}
