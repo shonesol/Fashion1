0ebebe2af7ac310df1658eccc84666fd71a83f10
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-14 10:57:42 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,14 +4,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD1__ACDdxUFjawiBr4_b6K42N-mbqe9_4",
-    authDomain: "fashionai-b443e.firebaseapp.com",
-    projectId: "fashionai-b443e",
-    storageBucket: "fashionai-b443e.firebasestorage.app",
-    messagingSenderId: "625810899658",
-    appId: "1:625810899658:web:5a0cc4cd4fbf26eaeeea33"
-};
+
 
 const app = initializeApp(firebaseConfig);
 
